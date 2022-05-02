5c58ef9c96da9781ba20c27ff6c61176ee22f654
DESKTOP-NGEPSM5\BEST <larisaoprea333@yahoo.com>
2022-05-02 10:04:18 -0700

    sidebar implementation

--- classroom-front/src/pages/Dashboard.js
+++ classroom-front/src/pages/Dashboard.js
@@ -1,8 +1,27 @@
 import React from "react";
 import axios from "axios";
 import Class from "./Class.js";
+import Sidebar from "react-sidebar";
+import Login from "./authentification/Login.js";
+import "../css/sidebar.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
 
 class Dashboard extends React.Component{
+   
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+          sidebarOpen: true
+        };
+        this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+      }
+    
+      onSetSidebarOpen(open) {
+        this.setState({ sidebarOpen: open });
+      }
 
   handleLogoutClick(){
     axios
@@ -20,11 +39,24 @@ class Dashboard extends React.Component{
   render(){
   return (
     <div>
+       
       <div>
+      <Sidebar
+        sidebar={ <ul> <li>Larisa</li> <li>Dani</li> <li>Alin</li> </ul>   }
+        open={this.state.sidebarOpen}
+        onSetOpen={this.onSetSidebarOpen}
+        styles={{ sidebar: { background: "white" } }}>  
+
+          <a onClick={() => this.onSetSidebarOpen(true)}>
+          <FontAwesomeIcon icon={faBars} />
+        </a> 
+      </Sidebar>
         <h1>Dashboard</h1>
         <h1>Status: {this.props.loggedInStatus}</h1>
         <button onClick={() => this.handleLogoutClick()}>Logout</button>
         <button>Create Class</button>
+      
+      
       </div>
       <Class/>
     </div>
