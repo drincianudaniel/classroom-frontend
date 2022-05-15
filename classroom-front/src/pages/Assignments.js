42265093c4c2ecd7d6e4b25e940bdb2a248f707d
DESKTOP-OVMJL3E\aling <alingavrilescu@yahoo.com>
2022-05-15 19:24:39 +0300

    assignments changes

--- classroom-front/src/pages/Assignments.js
+++ classroom-front/src/pages/Assignments.js
@@ -4,6 +4,8 @@ import "../css/assignments.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Link } from "react-router-dom";
 import { faClipboardUser } from '@fortawesome/free-solid-svg-icons'
+import {faEllipsisVertical} from '@fortawesome/free-solid-svg-icons'
+
 import { withRouter } from "react-router-dom";
 
 class Assignments extends React.Component {
@@ -15,39 +17,45 @@ class Assignments extends React.Component {
       }
 
       componentDidMount(){
-        this.getUsersData()
+        this.getAssignmentData()
     } 
     
-    getUsersData() {
+    deleteAssignment(id)
+    {
+        axios
+        .delete(`http://localhost:3000/deleteassignment/${id}`,{ withCredentials: true })
+        .then(res=>{
+            this.getAssignmentData();
+        })
+        .catch((error) => {
+            console.log(error)
+      })
+    }
+
+    getAssignmentData() {
         axios
             .get(`http://localhost:3000/usersassignments/${this.props.match.params.id}`, { withCredentials: true })
             .then(res => {
                 const data = res.data
                 console.log(res.data)
-                const assignments = data.map(u =>
-                //   <div class="assignments">
-                //   <p>{u.id}</p>
-                //   <p>{u.name}</p>
-                //   <p>{u.details}</p>
-                //   </div>
-                // <Link to={`/classpage/${u.id}`}>
-                <Link>
+                const assignments = data.map(u =>                  
                 <div class="container">
                     <div class="card3">
                         <div class="card-body row">
-                            <div class="assigclass2 col-lg-1"> 
+                            <div class="col-1 "> 
                                 <FontAwesomeIcon class="icon" icon={faClipboardUser} /> 
                             </div>
-                            
-                            <div class="texts col-lg-9">
-                                <h4>{u.name}</h4>
-                                {/* <h5>{u.details}</h5> */}
+                            <Link class="col-xl-10 col-lg-10 col-md-10">
+                                    <h5>X posted a new assignment: {u.name}</h5>
+                                    {/* <h5>TIME</h5> */}
+                            </Link>
+                            <div class="col-1">
+                            <FontAwesomeIcon class="icon2" icon={faEllipsisVertical} onClick={()=>this.deleteAssignment(u.id)}/> 
                             </div>
                         </div>
                     </div> 
                 </div>
-                </Link>
-                )
+                ).reverse();
                     this.setState({
                         assignments
                     })
