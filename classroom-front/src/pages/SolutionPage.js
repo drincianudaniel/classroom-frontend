492887a4c4614443224a90e52c6ab4708064aa26
DESKTOP-NGEPSM5\BEST <larisaoprea333@yahoo.com>
2022-05-18 11:11:21 -0700

    show assignments details and solution for assignment( when you are teacher/ student)

--- classroom-front/src/pages/SolutionPage.js
+++ classroom-front/src/pages/SolutionPage.js
@@ -9,18 +9,76 @@ class SolutionPage extends React.Component{
         super(props);
         this.state = {
           solution: [],
+          assignment: [],
           show: false,
-          ModalData: "",
-          details:""
+          solutions: [],
+          solution_content:"",
+          username: ""
         };
         console.log(this.props)
       }
 
       componentDidMount(){
         this.getSolutionData()
+        this.getAssignmentDetails()
+        this.getAllSolutionData()
     }
     
+    getAssignmentDetails(){
+        axios
+        .get(`http://localhost:3000/getassignment/${this.props.match.params.assignment_id}`, { withCredentials: true })
+        .then(res =>{
+            // const data = res.data
+            this.setState({
 
+                assignment: res.data.assignment
+
+            })
+            console.log(this.state.assignment)
+            
+        })
+        .catch((error) => {
+            console.log(error)
+         })
+    }
+    createSolution(){
+        axios
+        .post(`http://localhost:3000/createsolution`,
+        {
+               solution: {
+                   assignment_id: this.props.match.params.assignment_id,
+                   solution_content: this.state.solution_content
+               } 
+        },
+        { withCredentials: true })
+        .then(res =>{
+            this.getSolutionData()
+        })
+        .catch((error) => {
+            console.log(error)
+      })
+        
+    }
+
+    getAllSolutionData(){
+        axios
+        .get(`http://localhost:3000/allsolutions/${this.props.match.params.assignment_id}`, { withCredentials: true })
+        .then(res =>{
+            const data = res.data
+            const solutions = data.map(u =>
+              <div>  
+                  <p> {u.id} </p>
+                  <p>{u.user.name} </p>
+                  {u.solution_content}
+              </div>  
+              
+            )
+            console.log(this.state.username)
+            this.setState({
+                solutions
+            })
+        })
+    }
     getSolutionData(){
         axios
         .get(`http://localhost:3000/usersolutions/${this.props.match.params.assignment_id}`, { withCredentials: true })
@@ -28,7 +86,6 @@ class SolutionPage extends React.Component{
             const data= res.data.solutions
             const solution = data.map(u =>
                 <div>
-                    <div>{u.id }</div>
                     <div>{u.solution_content} </div>
                  </div>
                     
@@ -37,19 +94,33 @@ class SolutionPage extends React.Component{
                 solution
             })
         })
+        .catch((error) => {
+            console.log(error)
+      })
     }
-
-
 
     render(){ 
      return(
        
         <div>
-             {this.state.solution.length === 0 && <div>Submit solution</div>}
-             {this.state.solution}
+            {this.props.user.user_type === "Student" &&
+            <div> 
+                {this.state.assignment.name} <br></br>
+                {this.state.assignment.details}
+                {this.state.solution.length === 0 && 
+                    <div>
+                        <h1>Submit solution </h1>
+                        <span class="textarea input" role="textbox" contenteditable="true" onInput={e => this.setState({solution_content: e.currentTarget.innerText})} value={this.state.solution_content}></span>
+                        <button onClick={() =>this.createSolution()}>Submit solution</button>
+                    </div>}
+                {this.state.solution}
+             </div>}
+             {this.props.user.user_type === "Teacher" &&
+            <div>
+                {this.state.solutions}
+            </div>}
         </div>
     );}
       
-    
 }
 export default SolutionPage;
